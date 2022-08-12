cd50ec9be818351eb1ea9e37e86b38de1a06bdc4
Jonlyet <iamjeipark@gmail.com>
2022-08-12 15:10:44 +0900

    Programmers Variable

---
+++ Programmers beginners/src/javaStudy/VariableExam.java
@@ -0,0 +1,20 @@
+package javaStudy;
+
+public class VariableExam {
+
+	public static void main(String[] args) {
+		int count;
+		
+		count = 10; 
+		
+		count = 20; 
+		
+		System.out.println(count); // 값 20 출력
+		
+	//	count = 11.1;  정수이므로 실수 불가 
+		
+		double avg = 11.1; 
+		
+		String name = "carami"; 
+	}
+}
