66b6b72880cdfca89b78aa623453c0d307d3b2b2
Jonlyet <93205435+Jonlyet@users.noreply.github.com>
2022-08-12 16:26:07 +0900

    Create PrimitiveDataTypeExam.java

---
+++ Programmers beginners/src/javaStudy/PrimitiveDataTypeExam.java
@@ -0,0 +1,21 @@
+package javaStudy;
+
+public class PrimitiveDataTypeExam {
+
+	public static void main(String[] args) {
+		boolean isFun = true;   /// boolean -> true & false 값만 출력 
+		System.out.println(isFun);
+		
+		char c = 'A'; // char - > 문자 하나를 표현
+		
+		int x = 59; // 59 값 자체를 리터럴이라고 표현 
+		
+		long bing = 31321312L;  // long type은 반드시 뒤에 L을 넣어줘야 한다 
+		
+		float f = 32.1f; // 실수를 나타날때는 뒤에 f를 넣어줘야한다 
+		
+		double d = 312313.5; // 좀 더 큰 실수를 나타날때
+		
+		
+	}
+}
