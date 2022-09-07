1eecc9ce5a6d8fc3854b87a9822c64c05e297975
Jonlyet <iamjeipark@gmail.com>
2022-09-08 00:17:04 +0900

    참조타입

---
+++ Programmers beginners/src/javaStudy/ReferenceDataType.java
@@ -0,0 +1,11 @@
+package javaStudy;
+
+public class ReferenceDataType {
+
+	public static void main(String[] args) {
+			int i = 4; // 기본형 변수
+			
+			String str = new String("hello"); // 참조형 
+			
+	}
+}
