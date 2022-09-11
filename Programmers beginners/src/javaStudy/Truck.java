f8b8d70505261ab7d6c11c5086b2f2e3ef04ad4a
Jonlyet <iamjeipark@gmail.com>
2022-09-12 00:46:49 +0900

    super와 부모생성자

---
+++ Programmers beginners/src/javaStudy/Truck.java
@@ -0,0 +1,9 @@
+package javaStudy;
+
+public class Truck extends Car {
+	public Truck() {
+		super(); // 부모를 가르키는 키워드 
+		System.out.println("Truck의 기본 생성자 입니다.");
+	}
+	
+}
---
+++ Programmers beginners/src/javaStudy/TruckExam.java
@@ -0,0 +1,11 @@
+package javaStudy;
+
+public class TruckExam {
+	public static void main(String[] args) {
+		
+		Truck t = new Truck();
+		//super 키워드 : 부모 객체를 나타내는 키워드 
+		
+		
+	}
+}
