96854c954bae2206327105eadcf98b64a1692e2c
Jonlyet <iamjeipark@gmail.com>
2022-09-27 21:28:42 +0900

    람다

---
+++ Programmers Mid-level/src/javastudy/LamdaExam.java
@@ -0,0 +1,17 @@
+package javastudy;
+
+public class LamdaExam {
+
+	public static void main(String[] args) {
+		
+	
+	new Thread (()-> {
+
+			for(int i = 0; i < 10; i ++) {
+				System.out.println("hello");
+			}
+		
+		
+		}).start(); 
+	}
+}
